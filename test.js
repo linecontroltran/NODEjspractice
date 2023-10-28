a34e469113509c448c20dc927936ec3e31bb4ab1
Tom <linecontroltran@gmail.com>
2023-10-28 06:45:06 -0400

    Version 1

---
+++ test.js
@@ -0,0 +1,14 @@
+//const name = "mario";
+
+//console.log(name);
+
+//const name1 = "yoshi";
+
+//console.log(name1);
+
+const greet = (name) => {
+  console.log(`hello, ${name}`);
+};
+
+greet("mario");
+greet("yoshi");
